ee86e0fa4aa8386773c6c0b55cfe7e5f883593a7
Yingquan Zhao <yingquanzhao@zzzPro.local>
2018-07-09 14:22:54 +0800

    delete MF

--- src/controller/Instrumentor/RVInstrumentor.java
+++ src/controller/Instrumentor/RVInstrumentor.java
@@ -7,8 +7,21 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RVInstrumentor {
+
+    /**
+     * Constants
+     */
+    private static final String SLASH = "/";
+    private static final String INSTRUMENTATION_PACKAGES_DEFAULT = "default";
+
+    /**
+     *  packages and classes which needed to instrument or ignore
+     */
+    private static final Set<String> pckgPrefixesToIgnore = new HashSet<String>();
 
 
     /**
@@ -38,6 +51,29 @@ public class RVInstrumentor {
                 return new byte[0];
             }
         });
+    }
+
+    /**
+     *  通过类名，判断当前类是否需要被插桩
+     *
+     *  @param className
+     */
+    private static boolean shouldInstrumentClass(String className){
+
+        if(className == null)
+            return false;
+
+        String pckgName = INSTRUMENTATION_PACKAGES_DEFAULT;
+        int lastSlashIndex = className.lastIndexOf(SLASH);
+
+        //get the package name
+        if(lastSlashIndex != -1){
+
+            pckgName = className.substring(0,lastSlashIndex);
+        }
+
+
+        return true;
     }
 
 }
